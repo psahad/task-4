89f04a852d7c1324c0698352da88293e465344cb
Sahad <iamsahadp@gmail.com>
2022-01-24 22:35:37 +0530

    chart @media completed

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
         google.charts.load('current',{'packages':['corechart']});
-        google.charts.setOnLoadCallback(drawStuff);
+        google.charts.setOnLoadCallback(drawStuffDesktop);
 
-        function drawStuff(){
+        google.charts.setOnLoadCallback(drawStuffMobile);
+
+        function drawStuffDesktop(){
         var data = new google.visualization.arrayToDataTable([
           ['', 'Total Sales', 'Total Balance'],
           ['Mar 21', 450, 550],
@@ -57,6 +59,62 @@
         };
 
         var table = new google.visualization.ColumnChart(document.getElementById('chart'));
+
+        table.draw(data, options);
+        }
+        
+
+        function drawStuffMobile(){
+        var data = new google.visualization.arrayToDataTable([
+          ['', 'Total Sales', 'Total Balance'],
+          ['Mar 21', 450, 550],
+          ['Mar 22', 200, 250],
+          ['Mar 23', 600, 425]
+
+          
+        ]);
+
+        var options = {
+            bars: 'vertical',
+            vAxis: {format: 'Currency',
+                gridlines : {
+                    count : 3, color: '#282C38'
+                },
+                ticks: [200, 400, 600],
+                baseline : {
+                    color: '#282C38'
+                },
+                textStyle: {
+                    color: '#3C4254', 
+                    fontName: 'inter'
+                },
+            },
+            hAxis: {
+                textStyle: {
+                    color: '#3C4254', 
+                    fontName: 'inter'
+                }
+            },
+            height: 236,
+            width: 300,
+            backgroundColor: {
+                    fill: 'transparent',
+                },
+            chartArea: {
+                backgroundColor: {
+                    fill: 'transparent',
+                }
+            },
+            bar: {
+                groupWidth: 20
+            },
+            colors: ['#11CABE', '#246CF9'],
+            fontName: 'inter',
+            theme: 'material',
+            legend: 'none',
+        };
+
+        var table = new google.visualization.ColumnChart(document.getElementById('chart-mobile'));
 
         table.draw(data, options);
         }
